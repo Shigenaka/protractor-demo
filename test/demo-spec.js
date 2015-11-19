9a2c7ec4eb62ae2dfe3bde52f1dc8568c0cfe76d
Shigenaka <mshig19@uw.edu>
2015-11-19 10:14:04 -0800

    Update demo-spec.js

--- test/demo-spec.js
+++ test/demo-spec.js
@@ -1,2 +1,26 @@
 'use strict';
 
+describe('Twitter login form', function() {
+
+    beforeEach(function() {
+        browser.get('http://localhost:8080/');
+
+        var emailInput = element(by.model('email'));
+        emailInput.sendKeys('test@example.com');
+        var passwordInput = element(by.model('password'));
+        passwordInput.sendKeys('password');
+    })
+
+    it('should have a page title of "Twitter"', function() {
+        expect(browser.getTitle()).toEqual('Twitter');
+    });
+
+    it('should disable the sign-in button when invalid', function() {
+
+    })
+
+    it('should let the user sign in to an account', function() {
+        var signInButton = element(by.buttonText('Sign-in'));
+        signInButton.click();
+    })
+});
